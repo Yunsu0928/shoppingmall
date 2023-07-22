1fac2a9e5da377f9e62aa981eb60757ae14d4d28
hongyunsu <dbstn9@hanmail.net>
2023-07-22 20:57:41 +0900

    [:hammer:FEAT]: detail 페이지 input창 useEffect hook 사용 완료

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -14,6 +14,11 @@ import { Row, Col } from "react-bootstrap";
 function Detail({ clothes }) {
 	//유저가 URL파라미터에 입력한 내용을 가져오려면 useParams()를 사용
 	let { id } = useParams();
+	//useEffect 사용 시 사용하는 변수들
+	let [count, setCount] = useState(0);
+	let [alarm, setAlarm] = useState(true);
+	// input창에 숫자 외 글자 입력시 글자입력안된다는 메세지 띄우기
+	let [num, setNum] = useState("");
 
 	//console.log(id); // detail/1 1이 출력된다
 	let findProduct = clothes.find((e) => {
@@ -23,20 +28,42 @@ function Detail({ clothes }) {
 
 	// useEffect hook 사용 방법
 	useEffect(() => {
-		console.log("안녕"); // mount시 출력 즉, 페이지 로드 시
-	});
-	let [count, setCount] = useState(0);
+		// console.log("안녕"); // mount시 출력 즉, 페이지 로드 시
+		// 2초 뒤에 2초이벤트 창 안보이게 작업하기
+		let a = setTimeout(() => {
+			setAlarm(false);
+		}, 2000);
+		return () => {
+			// return은 useEffect가 실행되기 전에 먼저 실행된다
+			clearTimeout(a);
+		};
+	}, []);
+
+	useEffect(() => {
+		if (isNaN(num) === true) alert("숫자를 입력해주세요");
+	}, [num]);
 
 	return (
 		<div>
-			상세페이지입니다
-      {/* useEffect가 update시 출력 즉, 재렌더링 시 */}
+			<p>상세페이지입니다</p>
+			{/* input창에 숫자 외 글자 입력시 글자입력안된다는 메세지 띄우기 */}
+			<input
+				onChange={(e) => {
+					setNum(e.target.value);
+				}}
+			/>
+			<br />
+			{/* <div className="alert alert-warning">2초 이내 구매시 할인!</div> */}
+			{alert === true ? (
+				<div className="alert alert-warning">2초 이내 구매시 할인!</div>
+			) : null}
+			{/* useEffect가 update시 출력 즉, 재렌더링 시 */}
 			{count}
 			<button
 				onClick={() => {
 					setCount(count + 1);
 				}}
-			> 
+			>
 				버튼
 			</button>
 			<div>
