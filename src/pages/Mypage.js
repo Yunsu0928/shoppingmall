1cc2682157a5b71ab63d7c3256de1e86586ddc48
hongyunsu <dbstn9@hanmail.net>
2023-07-23 23:06:02 +0900

    [:construction:FEAT]: ajax fetch 작업중

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+
 function Mypage() {
-  return (
-    <div>
-      마이페이지 입니다
-    </div>
-  )
+	return (
+		<div>
+			마이페이지 입니다
+			<Outlet></Outlet>
+		</div>
+	);
 }
 
-export default Mypage
+export default Mypage;
